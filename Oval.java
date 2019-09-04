b1938c7fa175251ee78617d4663cddaaf10d01bc
hozumi <tvxqg5jyj@gmail.com>
2019-09-04 14:53:31 +0900

    位置が悪かったので修正したのと 名前がいっぱいあって汚かったので削除しました・。

--- Oval.java
+++ Oval.java
@@ -5,15 +5,6 @@
 <<<<<<< HEAD
  * @author (2018315057/마트바라 케이토,2018315058/오타 오아키,2018315054/호즈미 요시아키,)
  * @version (2018/9/4)
-=======
-<<<<<<< HEAD
- * @author (2018315058  오타 오아키 2018315054 호즈미 요시아키)
- * @version (2019/9/4)
-=======
- * @author (2018315057/matubara keito)
- * @version (バージョン番号もしくは日付)
->>>>>>> 72da7068011189fc690d31f04e0fe75ab948d72a
->>>>>>> f16159a818fd1c82a5f57ed464e10556dec8d4cf
  */
 public class Oval implements Shape
 {
